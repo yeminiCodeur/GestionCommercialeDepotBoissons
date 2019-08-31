c3ce5265decc843c85f14fe2a2ddc2f77249956d
Yemini Codeur <haidara.aboubacrine11@gmail.com>
2019-08-31 23:10:01 +0000

    creation de packages.

---
+++ src/main/java/org/sid/gestioncommercialdepotboisson/GestioncommercialdepotboissonApplication.java
@@ -0,0 +1,13 @@
+package org.sid.gestioncommercialdepotboisson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioncommercialdepotboissonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestioncommercialdepotboissonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sid/gestioncommercialdepotboisson/GestioncommercialdepotboissonApplicationTests.java
@@ -0,0 +1,16 @@
+package org.sid.gestioncommercialdepotboisson;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GestioncommercialdepotboissonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
